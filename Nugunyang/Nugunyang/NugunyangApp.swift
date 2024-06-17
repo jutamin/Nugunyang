2f93dae7725c5cbc6d2b1fe7c163a339cf3c871f
jutamin <wonjy0307@gmail.com>
2024-06-17 19:25:00 +0900

    xcode (재)생성

---
+++ Nugunyang/Nugunyang/NugunyangApp.swift
@@ -0,0 +1,17 @@
+//
+//  NugunyangApp.swift
+//  Nugunyang
+//
+//  Created by 원주연 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NugunyangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
